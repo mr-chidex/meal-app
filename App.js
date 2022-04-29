0d5cd0cafe2de9b0d5a9c1ea5889b677cb37f16c
mr-chidex <mbahchiboy2@gmail.com>
2022-04-29 11:52:47 +0100

    font added

--- App.js
+++ App.js
@@ -1,7 +1,30 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from "react";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import * as Font from "expo-font";
+import AppLoading from "expo-app-loading";
+
+const fetchFonts = () => {
+  return Font.loadAsync({
+    "OpenSans-Bold": require("./assets/fonts/OpenSans-Bold.ttf"),
+    OpenSans: require("./assets/fonts/OpenSans-Regular.ttf"),
+  });
+};
 
 export default function App() {
+  const [fontLoading, setFontLoading] = useState(false);
+  const [error, setError] = useState("");
+
+  if (!fontLoading) {
+    return (
+      <AppLoading
+        startAsync={fetchFonts}
+        onError={() => setError("error loading font")}
+        onFinish={() => setFontLoading(true)}
+      />
+    );
+  }
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
@@ -13,8 +36,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
